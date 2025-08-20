03d990a53a688e00cbfe5155011839a1b47ea3c8
Ezequiel Parziale <eparziale@gmail.com>
2025-08-20 18:23:09 -0300

    :zap: chore: set eslint configuration for consistency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,21 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   eslintPluginPrettierRecommended,
-];
+  {
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
