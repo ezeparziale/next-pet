5b184007f06d6a6dececc34a132fe708fd773706
Ezequiel Parziale <eparziale@gmail.com>
2025-12-09 09:07:18 -0300

    :recycle: refactor: simplify OS detection logic in useOperatingSystem hook

--- src/lib/hooks/use-operating-system.tsx
+++ src/lib/hooks/use-operating-system.tsx
@@ -1,24 +1,22 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
+
+const detectOS = () => {
+  if (typeof window === "undefined") {
+    return "Unknown"
+  }
+  const userAgent = window.navigator.userAgent
+  if (userAgent.indexOf("Win") !== -1) return "Windows"
+  if (userAgent.indexOf("Mac") !== -1) return "MacOS"
+  if (userAgent.indexOf("Linux") !== -1) return "Linux"
+  if (userAgent.indexOf("Android") !== -1) return "Android"
+  if (userAgent.indexOf("like Mac") !== -1) return "iOS"
+  return "Unknown"
+}
 
 export function useOperatingSystem() {
-  const [os, setOS] = useState<
-    "Windows" | "MacOS" | "Linux" | "Android" | "iOS" | "Unknown"
-  >("Unknown")
-
-  useEffect(() => {
-    const detectOS = () => {
-      const userAgent = window.navigator.userAgent
-      if (userAgent.indexOf("Win") !== -1) return "Windows"
-      if (userAgent.indexOf("Mac") !== -1) return "MacOS"
-      if (userAgent.indexOf("Linux") !== -1) return "Linux"
-      if (userAgent.indexOf("Android") !== -1) return "Android"
-      if (userAgent.indexOf("like Mac") !== -1) return "iOS"
-      return "Unknown"
-    }
-
-    // Initial detection
-    setOS(detectOS())
-  }, [])
+  const [os] = useState<"Windows" | "MacOS" | "Linux" | "Android" | "iOS" | "Unknown">(
+    detectOS,
+  )
 
   return {
     os,
